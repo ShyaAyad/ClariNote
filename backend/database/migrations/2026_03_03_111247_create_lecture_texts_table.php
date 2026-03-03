aa5367951bc4eb94a34d36546428dc38f0c826de
ShyaAyad <shyaayad1@gmail.com>
2026-03-03 14:48:15 +0300

    Adding migration files, models, and writing relationships

---
+++ backend/database/migrations/2026_03_03_111247_create_lecture_texts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Lecture;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lecture_texts', function (Blueprint $table) {
+            $table->id();
+            // indexing it for faster access
+            $table->foreignIdFor(Lecture::class)->constrained()->onDelete('cascade')->index();
+            $table->longText('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lecture_texts');
+    }
+};
