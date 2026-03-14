dbbec62088f50f47f943c0e1940d01e80b5047ac
ShyaAyad <shyaayad1@gmail.com>
2026-03-14 13:35:20 +0300

    Fetching all user lectures and displaying them in a stack list

--- frontend/ClariNote/src/components/Lectures.jsx
+++ frontend/ClariNote/src/components/Lectures.jsx
@@ -1,22 +1,35 @@
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Paper from "@mui/material/Paper";
-import Stack from "@mui/material/Stack";
+import {Box, Button, Paper, Stack, Typography} from "@mui/material";
 import { styled } from "@mui/material/styles";
-import UploadFileRoundedIcon from "@mui/icons-material/UploadFileRounded";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: (theme.vars ?? theme).palette.text.secondary,
-  ...theme.applyStyles("dark", {
-    backgroundColor: "#1A2027",
-  }),
-}));
+import {UploadFileRounded, PictureAsPdfRounded} from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import * as api from "../API/api.js";
 
 export default function Lectures() {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: "#fff",
+    ...theme.typography.body2,
+    padding: "12px 16px",
+    color: theme.palette.text.primary,
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    borderRadius: "8px",
+    boxShadow: "none",
+    border: "1px solid #e0e0e0",
+  }));
+
+  const [lectures, setLectuers] = useState([]);
+
+  const fetchLectures = async () => {
+    const res = await api.lectures();
+    const allLectures = res.data.data;
+    setLectuers(allLectures);
+  };
+
+  useEffect(() => {
+    fetchLectures();
+  }, []);
+
   const handleUpload = (e) => {
     e.preventDefault();
   };
@@ -27,15 +40,25 @@ export default function Lectures() {
         variant="contained"
         type="submit"
         sx={{ marginBottom: "15px" }}
-        startIcon={<UploadFileRoundedIcon />}
+        startIcon={<UploadFileRounded />}
       >
         Upload lecture
       </Button>
       <Box sx={{ width: "100%" }}>
         <Stack spacing={2}>
-          <Item>Item 1</Item>
-          <Item>Item 2</Item>
-          <Item>Item 3</Item>
+          {lectures.map((lecture, id) => (
+            <Item key={id}>
+              <PictureAsPdfRounded />
+              <Box sx={{display: "flex", flexDirection: "column"}}>
+                {lecture.title}
+                <Typography
+                  sx={{ fontWeight: "200", color: "gray" }}
+                >
+                  {lecture.created_at.substr(0, 10)}
+                </Typography>
+              </Box>
+            </Item>
+          ))}
         </Stack>
       </Box>
     </form>
