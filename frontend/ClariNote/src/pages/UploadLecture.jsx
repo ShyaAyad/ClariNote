d9b5763259b6da50d85e27319326a012617869fb
ShyaAyad <shyaayad1@gmail.com>
2026-03-14 13:36:13 +0300

    Post request to upload lectures

--- frontend/ClariNote/src/pages/UploadLecture.jsx
+++ frontend/ClariNote/src/pages/UploadLecture.jsx
@@ -1,16 +1,43 @@
-import Button from "@mui/material/Button";
-import Input from "@mui/material/Input";
-import Typography from "@mui/material/Typography";
+import { Button, Input, TextField, Typography } from "@mui/material";
 import UploadFileRoundedIcon from "@mui/icons-material/UploadFileRounded";
+import { uploadLecture } from "../API/api.js";
+import { useState } from "react";
 
 const UploadLecture = () => {
-  const handleUpload = () => {
-    console.log("Upload file here");
+  const [title, setTitle] = useState("");
+  const [file, setFile] = useState(null);
+
+  const handleUpload = async(e) => {
+    e.preventDefault();
+
+    try {
+      if (!title || !file) {
+        alert("Please provide both title and pdf file.");
+        return;
+      }
+
+      const res = await uploadLecture(title, file);
+      console.log("Uploaded file: ", res);
+    } catch (error) {
+      console.error("Error uploading lecture: ", error.response.data);
+    }
   };
   return (
     <form onSubmit={handleUpload}>
       <Typography variant="h5">Upload a pdf</Typography>
-      <Input type="file" />
+      <TextField
+        label="Title"
+        variant="outlined"
+        fullWidth
+        sx={{ margin: "12px 0" }}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <Input
+        type="file"
+        inputProps={{ accept: ".pdf" }}
+        onChange={(e) => setFile(e.target.files[0])}
+      />
       <Button
         variant="contained"
         type="submit"
